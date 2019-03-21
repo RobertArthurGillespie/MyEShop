724a1fe6c0fef35c3f2b9052fe4c3a8676134f37
Robert Gillespie <RobertArthurGillespie@outlook.com>
2019-03-21 14:09:00 -0500

    Added Basket and BasketItems database files

---
+++ MyEShop/MyEShop.Core/Models/Basket.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyEShop.Core.Models
+{
+    public class Basket:BaseEntity
+    {
+        //important to set it to virtual and ICollection so when entity framework loads it from database
+        //it will automatically load the basket items as well
+        public virtual ICollection<BasketItem> BasketItems { get; set; }
+
+        public Basket()
+        {
+            this.BasketItems = new List<BasketItem>();
+        }
+    }
+}
