e6ecd882f474452efbf4592e4a6328aeb365e930
Robert Arthur Gillespie <RobertArthurGillespie@outlook.com>
2019-03-22 21:04:24 -0500

    Added BasketController, Basket UI, Basket Dependency Injections, Basket Views

--- MyEShop/MyEShop.WebUI/App_Start/UnityConfig.cs
+++ MyEShop/MyEShop.WebUI/App_Start/UnityConfig.cs
@@ -2,6 +2,7 @@ using MyEShop.Core.Contracts;
 using MyEShop.Core.Models;
 using MyEShop.DataAccess.InMemory;
 using MyEShop.DataAccess.SQL;
+using MyEShop.Services;
 using System;
 
 using Unity;
@@ -48,6 +49,9 @@ namespace MyEShop.WebUI
             // container.RegisterType<IProductRepository, ProductRepository>();
             container.RegisterType<IRepository<Product>, SQLRepository<Product>>();
             container.RegisterType<IRepository<ProductCategory>, SQLRepository<ProductCategory>>();
+            container.RegisterType<IRepository<Basket>, SQLRepository<Basket>>();
+            container.RegisterType<IRepository<BasketItem>, SQLRepository<BasketItem>>();
+            container.RegisterType<IBasketService, BasketService>();
         }
     }
 }
