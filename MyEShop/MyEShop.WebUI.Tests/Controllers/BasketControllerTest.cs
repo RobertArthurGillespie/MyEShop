f1bc18eb827f197f2a6a9595b716b443d9559387
Robert Arthur Gillespie <RobertArthurGillespie@outlook.com>
2019-03-23 11:44:47 -0500

    added more testing methods and mockups

---
+++ MyEShop/MyEShop.WebUI.Tests/Controllers/BasketControllerTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyEShop.Core.Contracts;
+using MyEShop.Core.Models;
+using MyEShop.Services;
+using MyEShop.WebUI.Controllers;
+using MyEShop.WebUI.Tests.Mocks;
+
+namespace MyEShop.WebUI.Tests.Controllers
+{
+    [TestClass]
+    public class BasketControllerTest
+    {
+        [TestMethod]
+        public void CanAddBasketItem()
+        {
+            //setup
+            IRepository<Basket> baskets = new MockContext<Basket>();
+            IRepository<Product> products = new MockContext<Product>();
+
+            var httpContext = new MockHttpContext();
+
+            IBasketService basketService = new BasketService(products, baskets);
+            var controller = new BasketController(basketService);
+            controller.ControllerContext = new System.Web.Mvc.ControllerContext(httpContext, new System.Web.Routing.RouteData(), controller);
+            
+            //Act
+            
+            //basketService.AddToBasket(httpContext, "1");
+
+            controller.AddToBasket("1");
+            
+
+            Basket basket = baskets.Collection().FirstOrDefault();
+
+            //Assert
+
+            Assert.IsNotNull(basket);
+            Assert.AreEqual(1, basket.BasketItems.Count);
+            Assert.AreEqual("1", basket.BasketItems.ToList().FirstOrDefault().ProductId);
+        }
+
+        [TestMethod]
+        public void CanGetSummaryViewModel()
+        {
+            IRepository<Basket> baskets = new MockContext<Basket>();
+            IRepository<Product> products = new MockContext<Product>();
+
+            var httpContext = new MockHttpContext();
+        }
+    }
+}
--- MyEShop/MyEShop.WebUI.Tests/Controllers/HomeControllerTest.cs
+++ MyEShop/MyEShop.WebUI.Tests/Controllers/HomeControllerTest.cs
@@ -4,11 +4,33 @@ using System.Linq;
 using System.Text;
 using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyEShop.Core.Contracts;
+using MyEShop.Core.Models;
+using MyEShop.Core.ViewModels;
 using MyEShop.WebUI;
 using MyEShop.WebUI.Controllers;
 
 namespace MyEShop.WebUI.Tests.Controllers
 {
+
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void IndexPageDoesReturnProducts()
+        {
+            IRepository<Product> productContext = new Mocks.MockContext<Product>();
+            IRepository<ProductCategory> productCategoryContext = new Mocks.MockContext<ProductCategory>();
+            HomeController controller = new HomeController(productContext, productCategoryContext);
+
+            productContext.AddItem(new Product());
+
+            var result = controller.Index() as ViewResult;
+            var viewModel = (ProductListViewModel)result.ViewData.Model;
+
+            Assert.AreEqual(1, viewModel.Products.Count());
+        }
+    }
     /*[TestClass]
     public class HomeControllerTest
     {
