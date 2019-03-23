f4c431ebe1c909ebb2fd6d9cfb11eae9b09817e1
Robert Arthur Gillespie <RobertArthurGillespie@outlook.com>
2019-03-23 17:47:02 -0500

    Added the Customer model

--- MyEShop/MyEShop.WebUI.Tests/Controllers/BasketControllerTest.cs
+++ MyEShop/MyEShop.WebUI.Tests/Controllers/BasketControllerTest.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Linq;
+using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MyEShop.Core.Contracts;
 using MyEShop.Core.Models;
+using MyEShop.Core.ViewModels;
 using MyEShop.Services;
 using MyEShop.WebUI.Controllers;
 using MyEShop.WebUI.Tests.Mocks;
@@ -47,7 +49,27 @@ namespace MyEShop.WebUI.Tests.Controllers
             IRepository<Basket> baskets = new MockContext<Basket>();
             IRepository<Product> products = new MockContext<Product>();
 
+            products.AddItem(new Product() { ID = "1", Price = 10.00m });
+            products.AddItem(new Product() { ID = "2", Price = 5.00m });
+
+            Basket basket = new Basket();
+            basket.BasketItems.Add(new BasketItem() { ProductId = "1", Quantity = 2 });
+            basket.BasketItems.Add(new BasketItem() { ProductId = "2", Quantity = 1 });
+            baskets.AddItem(basket);
+
+            IBasketService basketService = new BasketService(products, baskets);
+
+            var controller = new BasketController(basketService);
+
             var httpContext = new MockHttpContext();
+            httpContext.Request.Cookies.Add(new System.Web.HttpCookie("eCommerceBasket") { Value = basket.ID });
+            controller.ControllerContext = new System.Web.Mvc.ControllerContext(httpContext, new System.Web.Routing.RouteData(), controller);
+
+            var result = controller.BasketSummary() as PartialViewResult;
+            var basketSummary = (BasketSummaryViewModel)result.ViewData.Model;
+
+            Assert.AreEqual(3, basketSummary.BasketCount);
+            Assert.AreEqual(25.00m, basketSummary.BasketTotal);
         }
     }
 }
--- MyEShop/MyEShop.WebUI/Controllers/AccountController.cs
+++ MyEShop/MyEShop.WebUI/Controllers/AccountController.cs
@@ -155,6 +155,9 @@ namespace MyEShop.WebUI.Controllers
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
+                    //register the customer model
+
+
                     await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
                     
                     // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
